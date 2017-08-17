86029eb4da7ae977c0fd997ca69ffd9d63a99d5b
kevinfox <kevinfox@kainos.com>
2017-08-17 13:44:47 +0100

    Added functionality for user stories 1 and 2

--- db.js
+++ db.js
@@ -12,12 +12,48 @@ db.connect(function(err) {
 	console.log("Connected to MySql");
 });
 
-exports.getEmployees = function(id, callback)
-	{
-		db.query("SELECT employee_id AS id, fname as firstName, lname AS lastName, street AS address, town, postcode, iban, nin, salary FROM employee WHERE employee_id = ?",
-			[id],
+exports.getEmployeeDepartments = function(callback)
+{
+		db.query("SELECT * FROM employee NATURAL JOIN employee_department NATURAL JOIN department",
+			[],
 			function(err, rows){
 				if(err) throw err;
 				callback(rows);
 			});
-	}
+}
+
+//get All employees
+exports.getEmployees = function(id, callback)
+{
+		db.query("SELECT employee_id AS id, fname as firstName, lname AS lastName, street AS address, town, postcode, iban, nin, salary FROM employee",
+			[],
+			function(err, rows){
+				if(err) throw err;
+				callback(rows);
+			});
+}
+
+//get All Departments
+exports.getDepartment = function(callback)
+{
+		db.query("SELECT department_id, name FROM department",
+			[],
+			function(err, rows){
+				if(err) throw err;
+				callback(rows);
+			});
+}
+
+//insert new employee
+exports.insertEmployee = function (firstName, lastName, address, town, postcode, iban, nin, salary, callback)
+{
+    db.query(
+        "INSERT INTO employee (fname, lname, street, town, postcode, iban, nin, salary) " +
+        "VALUES (?,?,?,?,?,?,?,?)",
+        [firstName, lastName, address, town, postcode, iban, nin, salary],
+        function(err){
+            if(err) throw err;
+            callback("Insert Employee successful.");
+        }
+    );
+};
--- index.js
+++ index.js
@@ -1,15 +1,64 @@
 const express = require('express');
 const db = require('./db.js');
+const app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 
+//DB functions
 db.getEmployees(1, function(rows) {
 	employees = rows;
 });
 
-const app = express();
+db.getEmployeeDepartments(function(rows) {
+	employeeDepartments = rows;
+});
 
+db.getDepartment(function(rows) {
+	departments = rows;
+});
+
+//APP functions
+
+//Insert New Employee
+app.post('/employee', function(req, res){
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const address = req.body.address;
+    const town = req.body.town;
+    const postcode = req.body.postcode;
+    const iban = req.body.iban;
+    const nin = req.body.nin;
+    var salary = req.body.salary;
+    console.log('Salary is ' + salary);
+    salary = parseInt(salary);
+    console.log('Parsed int ' + salary);
+
+    if(firstName && lastName && address && town && postcode && iban && nin && salary){
+        db.insertEmployee(firstName, lastName, address, town, postcode, iban, nin, salary, function(message){
+            res.send(message);
+        });
+    }
+});
+
+
+//Get All Employees
 app.get('/employees', function (req,res) {
 	res.send(employees);
+	// body...
+});
+
+//Get Employee departments
+app.get('/employeeDepartment', function (req,res) {
+	res.send(employeeDepartments);
+	// body...
+});
+
+//Get Departments
+app.get('/department', function (req,res) {
+	res.send(departments);
 	// body...
 });
 
